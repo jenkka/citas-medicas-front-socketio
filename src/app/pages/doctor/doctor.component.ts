ef0fac76ea6eb14dcf263421e3097890221129d8
Erick Gamboa Valle <is710115@iteso.mx>
2021-12-03 18:05:12 -0600

    homepage and forms in doctors appointment v2

--- src/app/pages/doctor/doctor.component.ts
+++ src/app/pages/doctor/doctor.component.ts
@@ -14,6 +14,7 @@ import { NgbModal, ModalDismissReasons, NgbDateStruct, NgbDate } from '@ng-boots
 })
 
 export class DoctorComponent implements OnInit {
+  time = {hour: 13, minute: 30};
   model: NgbDateStruct | undefined; ;
   closeResult = '';
   doctor: Doctor | undefined;
