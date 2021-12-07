20809be799d1bb495866dc34b41d559535320434
JCGG <jcglez98@gmail.com>
2021-12-06 19:58:55 -0600

    Fix CORS

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 app.use(express.static('./dist/citas-medicas-front'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/citas-medicas-front/'}),
 );
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
